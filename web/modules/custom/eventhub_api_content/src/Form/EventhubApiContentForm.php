2595f25a570fe55b6786e3c4eaefd7403b9e21dd
{SebastienDaled} <{sebadale@student.arteveldehs.be}>
2024-03-12 09:41:15 +0100

    Controll on data that is coming from the API

--- web/modules/custom/eventhub_api_content/src/Form/EventhubApiContentForm.php
+++ web/modules/custom/eventhub_api_content/src/Form/EventhubApiContentForm.php
@@ -52,16 +52,42 @@ class EventhubApiContentForm extends FormBase {
   
     // for each event item received from the API, create a node
     foreach ($data->events as $event) {
-      // make a bolean for the sales status
+      $name = $event->name;
+      $address = $event->_embedded->venues[0]->address->line1;
+      $body = $event->description;
+      $city = $event->_embedded->venues[0]->city->name;
+      $country = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadByProperties(['name' => $event->_embedded->venues[0]->country->name]);
+      $date = date('Y-m-d\TH:i:s', strtotime($event->dates->start->localDate . ' ' . $event->dates->start->localTime));
+      $end_sale_date = date('Y-m-d\TH:i:s', strtotime($event->sales->public->endDateTime));
+      $genre = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadByProperties(['name' => $event->classifications[0]->genre->name]);
+      $hero_image_source = $event->images[0]->url;
+      $location = $event->_embedded->venues[0]->name;
+      $max_price = $event->priceRanges[0]->max;
+      $min_price = $event->priceRanges[0]->min;
+      $performer = $event->_embedded->attractions[0]->name;
       $onsale = false;
+      $seatmap = null;
+      $field_start_sale_date = date('Y-m-d\TH:i:s', strtotime($event->sales->public->startDateTime));
 
-      if ($event->dates->status->code == 'onsale') {
-        $onsale = true;
+      // check if event has a name
+      if (empty($name)) {
+        $name = 'No name';
+      }
+      
+      // check if event has an address
+      if (empty($address)) {
+        $address = 'No address';
       }
 
-      // get the country and genre taxonomy terms to check of they exist
-      $country = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadByProperties(['name' => $event->_embedded->venues[0]->country->name]);
-      $genre = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadByProperties(['name' => $event->classifications[0]->genre->name]);
+      // check if event has a description
+      if (empty($body)) {
+        $body = 'No description';
+      }
+
+      // check if event has a city
+      if (empty($city)) {
+        $city = 'No city';
+      }
 
       // does the check if the taxonomy terms exist, if not, create them
       if (empty($country)) {
@@ -75,7 +101,17 @@ class EventhubApiContentForm extends FormBase {
         $country = array_values($country)[0];
       }
 
-      // does the check if the taxonomy terms exist, if not, create them
+      // check if event has a date
+      if (empty($date)) {
+        $date = 'No date';
+      }
+
+      // check if event has an end sale date
+      if (empty($end_sale_date)) {
+        $end_sale_date = 'No end sale date';
+      }
+
+      // check if event has a location
       if (empty($genre)) {
         // taxonomy term does not exist, create it
         $genre = \Drupal\taxonomy\Entity\Term::create([
@@ -85,6 +121,36 @@ class EventhubApiContentForm extends FormBase {
         $genre->save();
       } else {
         $genre = array_values($genre)[0];
+      }
+
+      // check if event has a hero image
+      if (empty($hero_image_source)) {
+        $hero_image_source = 'https://via.placeholder.com/300';
+      }
+
+      // check if event has a location
+      if (empty($location)) {
+        $location = 'No location';
+      }
+
+      // check if event has a max price
+      if (empty($max_price)) {
+        $max_price = 'No max price';
+      }
+
+      // check if event has a min price
+      if (empty($min_price)) {
+        $min_price = 'No min price';
+      }
+
+      // check if event has a performer
+      if (empty($performer)) {
+        $performer = 'No performer';
+      }
+
+      // make a bolean for the sales status
+      if ($event->dates->status->code == 'onsale') {
+        $onsale = true;
       }
 
       // searches for the biggets image in size to use as the hero image
@@ -99,37 +165,44 @@ class EventhubApiContentForm extends FormBase {
         }
       }
 
-      $seatmap = null; 
-
+      // get the url of seatmap
       if (empty($seatmap)) {
-        $seatmap = 'https://via.placeholder.com/500';
+        $seatmap = 'https://via.placeholder.com/300';
       } else {
         $seatmap = $event->seatmap->staticUrl;
+      }
+
+      // check if event has a start sale date
+      if (empty($field_start_sale_date)) {
+        $field_start_sale_date = 'No start sale date';
       }
 
       // create the node
       $node = \Drupal\node\Entity\Node::create([
         'type' => 'event',
-        'title' => $event->name,
-        'field_adress' => $event->_embedded->venues[0]->address->line1,
-        'body' => $event->description,
-        'field_city' => $event->_embedded->venues[0]->city->name,
+        'title' => $name,
+        'field_adress' => $address,
+        'body' => [
+          'value' => $body,
+          'format' => 'full_html',
+        ],
+        'field_city' => $city,
         'field_country' => [
           'target_id' => $country->id(),
         ],
-        'field_date' => date('Y-m-d\TH:i:s', strtotime($event->dates->start->localDate . ' ' . $event->dates->start->localTime)),
-        'field_end_sale_date' => date('Y-m-d\TH:i:s', strtotime($event->sales->public->endDateTime)),
+        'field_date' => $date,
+        'field_end_sale_date' => $end_sale_date,
         'field_genre' => [
           'target_id' => $genre->id(),
         ],
-        'field_hero_image_source' => $event->images[$biggest_key]->url,
-        'field_location' => $event->_embedded->venues[0]->name,
-        'field_max_price' => $event->priceRanges[0]->max,
-        'field_min_price' => $event->priceRanges[0]->min,
-        'field_performer' => $event->_embedded->attractions[0]->name,
+        'field_hero_image_source' => $hero_image_source,
+        'field_location' => $location,
+        'field_max_price' => $max_price,
+        'field_min_price' => $min_price,
+        'field_performer' => $performer,
         'field_sales_status' => $onsale,
         'field_seatmap_image_source' => $seatmap,
-        'field_start_sale_date' => date('Y-m-d\TH:i:s', strtotime($event->sales->public->startDateTime)),
+        'field_start_sale_date' => $field_start_sale_date,
       ]);
       $node->save();
     }
